8b7c306fa4c08cf7326a341fff5e47fc2bb1a588
Dmitriy Vasilev <dima.vasiliev2002@mail.ru>
2023-03-05 16:50:41 +0700

    Экран регистрации

--- app/src/main/java/com/example/schedule/screens/LessonScreen.kt
+++ app/src/main/java/com/example/schedule/screens/LessonScreen.kt
@@ -3,7 +3,10 @@ package com.example.schedule.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
@@ -11,12 +14,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.schedule.Navigation
 import com.example.schedule.data.Lesson
 import com.example.schedule.ui.theme.Gray200
 import com.example.schedule.ui.theme.ScheduleTheme
